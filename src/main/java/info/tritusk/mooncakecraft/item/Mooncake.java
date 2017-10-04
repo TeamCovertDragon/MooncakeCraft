5766c2a3e9293be176b1694adcd30d9dd7589c9b
3TUSK <urey.s.knowledge@gmail.com>
2017-10-03 20:45:14 -0700

    License header

--- src/main/java/info/tritusk/mooncakecraft/item/Mooncake.java
+++ src/main/java/info/tritusk/mooncakecraft/item/Mooncake.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2017 3TUSK, Seraph-JACK, et al.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 package info.tritusk.mooncakecraft.item;
 
 import net.minecraft.item.ItemFood;
