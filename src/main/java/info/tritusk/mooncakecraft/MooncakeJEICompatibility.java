97eebe91a87d42159dcbe5eee7191aecbb533caa
3TUSK <urey.s.knowledge@gmail.com>
2017-10-04 21:15:59 -0700

    Special JEI compatibility, used for hiding "bad mooncake" (aka the fallback mooncake)

---
+++ src/main/java/info/tritusk/mooncakecraft/MooncakeJEICompatibility.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2017 3TUSK, Seraph-JACK, yesterday17, et al.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package info.tritusk.mooncakecraft;
+
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.IModRegistry;
+import mezz.jei.api.JEIPlugin;
+import mezz.jei.api.ingredients.IIngredientBlacklist;
+import net.minecraft.item.ItemStack;
+
+@JEIPlugin
+public class MooncakeJEICompatibility implements IModPlugin {
+    @Override
+    public void register(IModRegistry registry) {
+        IIngredientBlacklist blacklist = registry.getJeiHelpers().getIngredientBlacklist();
+        blacklist.addIngredientToBlacklist(new ItemStack(MooncakeConstants.MOONCAKE_FILLING, 1, 0));
+        blacklist.addIngredientToBlacklist(new ItemStack(MooncakeConstants.UNSHAPED_MOONCAKE_ITEM, 1, 0));
+        blacklist.addIngredientToBlacklist(new ItemStack(MooncakeConstants.RAW_MOONCAKE_ITEM, 1, 0));
+        blacklist.addIngredientToBlacklist(new ItemStack(MooncakeConstants.MOONCAKE_ITEM, 1, 0));
+    }
+}
