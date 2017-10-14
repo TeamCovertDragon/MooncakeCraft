e3468b98ece71aa3cf310dc5d1c78dcd771cb891
Yesterday17 <t@yesterday17.cn>
2017-10-15 03:35:35 +0800

    Remove IRecipe and uses IRecipeFactory & Recipe files instead.

---
+++ src/main/java/info/tritusk/mooncakecraft/crafting/RemainBottleRecipeFactory.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2017 3TUSK, Seraph-JACK, yesterday17, et al.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package info.tritusk.mooncakecraft.crafting;
+
+import com.google.gson.JsonObject;
+import net.minecraft.init.Items;
+import net.minecraft.inventory.InventoryCrafting;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.crafting.ShapelessRecipes;
+import net.minecraft.util.NonNullList;
+import net.minecraftforge.common.crafting.IRecipeFactory;
+import net.minecraftforge.common.crafting.JsonContext;
+import net.minecraftforge.oredict.ShapelessOreRecipe;
+
+public class RemainBottleRecipeFactory implements IRecipeFactory{
+
+    @Override
+    public IRecipe parse(JsonContext context, JsonObject json) {
+        ShapelessOreRecipe recipe = ShapelessOreRecipe.factory(context, json);
+
+        return new RemainBottleRecipe("mooncakecraft:remain_bottle", recipe.getRecipeOutput(), recipe.getIngredients());
+    }
+
+    public static class RemainBottleRecipe extends ShapelessRecipes {
+        public RemainBottleRecipe(String group, ItemStack output, NonNullList<Ingredient> ingredients) {
+            super(group, output, ingredients);
+        }
+
+        @Override
+        public NonNullList<ItemStack> getRemainingItems(InventoryCrafting inv)
+        {
+            NonNullList<ItemStack> nonnulllist = NonNullList.<ItemStack>withSize(inv.getSizeInventory(), ItemStack.EMPTY);
+
+            for (int i = 0; i < nonnulllist.size(); ++i)
+            {
+                ItemStack itemstack = inv.getStackInSlot(i);
+
+                if(itemstack.isItemEqual(new ItemStack(Items.POTIONITEM))) {
+                    nonnulllist.set(i, new ItemStack(Items.GLASS_BOTTLE, 1));
+                    continue;
+                }
+
+                nonnulllist.set(i, net.minecraftforge.common.ForgeHooks.getContainerItem(itemstack));
+            }
+
+            return nonnulllist;
+        }
+    }
+}
